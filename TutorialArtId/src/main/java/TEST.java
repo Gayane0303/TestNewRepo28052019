21e4ebd9eb03c46a1a3aee63fba3aa792f4d0821
gayane <gayanestdev@gmail.com>
2019-05-28 17:45:49 +0400

    write soem hint test 3

---
+++ TutorialArtId/src/main/java/TEST.java
@@ -0,0 +1,10 @@
+public class TEST {
+
+    public static void main (String []args){
+        int count = 50;
+        while  (count <=80){
+            System.out.println("Value are " + count);
+            count ++;
+        }
+    }
+}
